0b84e2a0ba786297cd860e9988f9837fc0df86d6
Prénom NOM <prenom.nom@ens.uvsq.fr>
2021-01-07 10:23:11 +0100

    elisabeth test

---
+++ exercises/TD03_fonctions/TD3.py
@@ -0,0 +1,12 @@
+nombre = int(input("donnez un nombre"))
+if (nombre > 11 and nombre < 1000) :
+    print("Gagné")
+
+
+n = 6
+for i in range (1, n+1) :
+    print(("a" * (n-1))+"b")
+
+
+
+
---
+++ exercises/TD04_listes/syracuse.py
@@ -0,0 +1,8 @@
+def syracuse(n):
+    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
+    #reponce : screanshot tablette
+    #3 9 10, 5, 25 26,
+    pass
+
+
+print(syracuse(3))
